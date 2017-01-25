2ced71b5ec2c31446358c7f7f6820d3d75e36e0f
Philip <phucvd.ce@gmail.com>
2017-01-25 11:26:01 +0700

    Update vstsinstall.sh

--- preqs/vstsinstall.sh
+++ preqs/vstsinstall.sh
@@ -9,4 +9,6 @@ wget https://github.com/Microsoft/vsts-agent/releases/download/v2.111.1/vsts-age
 mkdir myagent && cd myagent
 tar zxvf ../vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz
 
-./config.sh
+$tokenvsts=$1
+
+./config.sh --unattended --acceptteeeula --url https://philipvan.visualstudio.com --auth PAT --token $tokenvsts --pool default --agent dockerbuid01
