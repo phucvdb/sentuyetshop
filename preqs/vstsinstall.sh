7dc7e7c3c98781fcec2f9fc599e41761c82c65c9
Philip <phucvd.ce@gmail.com>
2017-01-25 11:05:31 +0700

    Create vstsinstall.sh

---
+++ preqs/vstsinstall.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+sudo apt-get install -y libunwind8 libcurl3 -y
+sudo apt-add-repository ppa:git-core/ppa -y
+sudo apt-get update -y
+sudo apt-get install git -y
+
+wget https://github.com/Microsoft/vsts-agent/releases/download/v2.111.1/vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz
+mkdir myagent && cd myagent
+tar zxvf ../vsts-agent-ubuntu.16.04-x64-2.111.1.tar.gz
+
+./config.sh
